ba510366a139f6f8c93506658882100e43ecfae1
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2025-12-06 22:31:06 +0100

    Remove unused import for UsuariosController in UsuariosServiceApplication

--- usuarios-service/src/main/java/com/gamelisto/usuarios_service/UsuariosServiceApplication.java
+++ usuarios-service/src/main/java/com/gamelisto/usuarios_service/UsuariosServiceApplication.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.gamelisto.usuarios_service.infrastructure.api.rest.UsuariosController;
-
 @SpringBootApplication
 public class UsuariosServiceApplication {
 
