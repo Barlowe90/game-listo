00846bbfa878fad2115547170b7c631b70d1886f
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2025-12-06 22:30:48 +0100

    Add logging to UsuariosServiceApplication for startup confirmation

--- usuarios-service/src/main/java/com/gamelisto/usuarios_service/UsuariosServiceApplication.java
+++ usuarios-service/src/main/java/com/gamelisto/usuarios_service/UsuariosServiceApplication.java
@@ -1,13 +1,21 @@
 package com.gamelisto.usuarios_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.gamelisto.usuarios_service.infrastructure.api.rest.UsuariosController;
 
 @SpringBootApplication
 public class UsuariosServiceApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(UsuariosServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(UsuariosServiceApplication.class, args);
+
+		logger.info("✅ === Microservicio usuarios funcionando === ");
 	}
 
 }
