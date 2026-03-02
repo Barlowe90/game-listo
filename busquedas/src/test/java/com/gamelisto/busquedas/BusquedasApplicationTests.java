59701d3de82a8fc4e495437ef9ad8d9116915188
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2026-03-02 23:33:24 +0100

    base busquedas

---
+++ busquedas/src/test/java/com/gamelisto/busquedas/BusquedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamelisto.busquedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusquedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
