a60afd006000c22026b090f66d3cd3d491fadb13
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2026-03-26 23:06:17 +0100

    fix: elimino dto buscar usuario por estad por no usarlo

--- backend/usuarios/src/main/java/com/gamelisto/usuarios/application/dto/BuscarUsuariosPorEstadoCommand.java
+++
@@ -1,5 +0,0 @@
-package com.gamelisto.usuarios.application.dto;
-
-import com.gamelisto.usuarios.domain.usuario.EstadoUsuario;
-
-public record BuscarUsuariosPorEstadoCommand(EstadoUsuario estado) {}
