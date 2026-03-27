ce400c48e78442f9443371ff1c274095286bb145
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2026-03-27 19:24:36 +0100

    feat: elimino campo discord id de publicaciones
    (cherry picked from commit 4a99c41a0ab277bc7aa6ee2d5cde457f0a226c54)

--- backend/publicaciones/src/main/java/com/gamelisto/publicaciones/domain/UsuarioRef.java
+++ backend/publicaciones/src/main/java/com/gamelisto/publicaciones/domain/UsuarioRef.java
@@ -11,29 +11,18 @@ public class UsuarioRef {
   private final UUID id;
   private final String username;
   private final String avatar;
-  private final String discordUserId;
 
-  private UsuarioRef(UUID id, String username, String avatar, String discordUserId) {
+  private UsuarioRef(UUID id, String username, String avatar) {
     this.id = id;
     this.username = username;
     this.avatar = avatar;
-    this.discordUserId = discordUserId;
   }
 
   public static UsuarioRef create(UUID id, String username, String avatar) {
-    return create(id, username, avatar, null);
+    return new UsuarioRef(id, username, avatar);
   }
 
   public static UsuarioRef reconstitute(UUID id, String username, String avatar) {
-    return reconstitute(id, username, avatar, null);
-  }
-
-  public static UsuarioRef create(UUID id, String username, String avatar, String discordUserId) {
-    return new UsuarioRef(id, username, avatar, discordUserId);
-  }
-
-  public static UsuarioRef reconstitute(
-      UUID id, String username, String avatar, String discordUserId) {
-    return new UsuarioRef(id, username, avatar, discordUserId);
+    return new UsuarioRef(id, username, avatar);
   }
 }
