582392574860a222041b73dec249a763f2eedc05
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2026-04-05 15:23:49 +0200

    feta: ahora funciona para aws

--- backend/publicaciones/src/main/java/com/gamelisto/publicaciones/domain/GrupoJuego.java
+++ backend/publicaciones/src/main/java/com/gamelisto/publicaciones/domain/GrupoJuego.java
@@ -6,26 +6,27 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Getter
 @ToString
 public class GrupoJuego {
-  private final GrupoId id;
-  private final PublicacionId publicacionId;
-  private final Instant fechaCreacion;
+    private final GrupoId id;
+    private final PublicacionId publicacionId;
+    private final Instant fechaCreacion;
 
-  private GrupoJuego(GrupoId id, PublicacionId publicacionId, Instant fechaCreacion) {
-    this.id = id;
-    this.publicacionId = publicacionId;
-    this.fechaCreacion = fechaCreacion;
-  }
+    private GrupoJuego(GrupoId id, PublicacionId publicacionId, Instant fechaCreacion) {
+        this.id = id;
+        this.publicacionId = publicacionId;
+        this.fechaCreacion = fechaCreacion;
+    }
 
-  public static GrupoJuego create(PublicacionId publicacionId) {
-    return new GrupoJuego(GrupoId.of(java.util.UUID.randomUUID()), publicacionId, Instant.now());
-  }
+    public static GrupoJuego create(PublicacionId publicacionId) {
+        return new GrupoJuego(GrupoId.of(UUID.randomUUID()), publicacionId, Instant.now());
+    }
 
-  public static GrupoJuego reconstitute(
-      GrupoId id, PublicacionId publicacionId, Instant fechaCreacion) {
-    return new GrupoJuego(id, publicacionId, fechaCreacion);
-  }
+    public static GrupoJuego reconstitute(
+            GrupoId id, PublicacionId publicacionId, Instant fechaCreacion) {
+        return new GrupoJuego(id, publicacionId, fechaCreacion);
+    }
 }
